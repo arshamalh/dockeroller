0f4d5187807b0e7b9d8b2963ac45404d2e81184b
Arsham <Arshamalh@gmail.com>
2022-09-17 16:57:44 +0430

    Fix: session nil pointer.

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -3,10 +3,14 @@ package session
 type session map[int64]map[string]any
 
 func New() *session {
-	return &session{}
+	ssn := make(session)
+	return &ssn
 }
 
 func (s *session) Set(chat_id int64, key string, value any) {
+	if (*s)[chat_id] == nil {
+		(*s)[chat_id] = make(map[string]any)
+	}
 	(*s)[chat_id][key] = value
 }
 
