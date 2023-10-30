24887f4c28a7ebdef5eded88d412484aa567b2a0
Arsham Arya <Arshamalh@gmail.com>
2023-10-31 02:03:15 +0330

    rm: extra files of old session.

--- contracts/session.go
+++
@@ -1,7 +0,0 @@
-package contracts
-
-type Session interface {
-	Set(chat_id int64, key string, value any)
-	Get(chat_id int64, key string) any
-	Del(chat_id int64, key string)
-}
--- tools/load_yaml_config.go
+++ tools/load_yaml_config.go
@@ -1,15 +1,3 @@
 package tools
 
-import (
-	"github.com/arshamalh/dockeroller/contracts"
-)
-
-type Configs struct {
-	TelegramInfo *contracts.Config
-}
-
-// Search for a dockeroller.yml file that includs needed configurations and load it.
-func LoadYamlConfig() (Configs, error) {
-	// TODO: Implement
-	return Configs{}, nil
-}
+// Search for a dockeroller.yml file that includes needed configurations and load it.
