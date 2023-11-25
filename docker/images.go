7f204217a196fc1b4ea22055bf0300358a04cb11
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-11-25 21:49:00 +0330

    fix: set the status of image

--- docker/images.go
+++ docker/images.go
@@ -39,26 +39,23 @@ func (d *docker) ImageRemove(ctx context.Context, imageID string, force, pruneCh
 }
 
 func (d *docker) getImageStatus(ctx context.Context, image types.ImageSummary) (status string) {
-	if len(image.RepoTags) == 0 {
+	if image.RepoTags[0] == "<none>:<none>" {
 		status = string(entities.ImageStatusUnUsedDangling)
+		return
 	}
 
-	containers, _ := d.cli.ContainerList(ctx, types.ContainerListOptions{})
-	newImgs := make(map[string][]string)
-	for _, cont := range containers {
-		if cont.ImageID != image.ID {
-			status = string(entities.ImageStatusUnUsed)
-		} else {
-			newSlice := newImgs[image.ID]
-			if newSlice == nil {
-				newSlice = make([]string, 0)
-			}
-			newSlice = append(newSlice, cont.ID)
-			newImgs[image.ID] = newSlice
+	if image.Containers == 0 {
+		status = string(entities.ImageStatusUnUsed)
+		return
+	}
 
+	containers, _ := d.cli.ContainerList(ctx, types.ContainerListOptions{All: true, Latest: true})
+	for _, cont := range containers {
+		if cont.ImageID == image.ID {
 			status = string(entities.ImageStatusInUse)
+			return
 		}
 	}
 
-	return
+	return string(entities.ImageStatusUnUsed)
 }
